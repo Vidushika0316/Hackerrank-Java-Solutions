5758101dcdbeecd7276b783e58ae145e9756ac61
Vidushika Dasanayaka <vidushi0316@gmail.com>
2021-09-30 02:34:23 +0530

    Solution for Java Currency Formatter

---
+++ Java Currency Formatter/Solution.java
@@ -0,0 +1,57 @@
+/*
+Java Currency Formatter
+
+Given a double-precision number, payment , denoting an amount of money, use the NumberFormat class' getCurrencyInstance method to convert payment  into the US, Indian, Chinese, and French currency formats. Then print the formatted values as follows:
+
+US: formattedPayment
+India: formattedPayment
+China: formattedPayment
+France: formattedPayment
+
+Input Format
+
+A single double-precision number denoting payment.
+
+Sample Input
+
+12324.134
+Sample Output
+
+US: $12,324.13
+India: Rs.12,324.13
+China: ￥12,324.13
+France: 12 324,13 €
+Explanation
+
+Each line contains the value of payment formatted according to the four countries' respective currencies.
+*/
+
+import java.util.Scanner;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class Solution {
+    
+    public static void main(String[] args) {
+        /* Read input */
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        /* Create custom Locale for India. 
+          I used the "IANA Language Subtag Registry" to find India's country code */
+        Locale indiaLocale = new Locale("en", "IN");
+
+        /* Create NumberFormats using Locales */
+        NumberFormat us     = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat india  = NumberFormat.getCurrencyInstance(indiaLocale);
+        NumberFormat china  = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        NumberFormat france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+
+        /* Print output */        
+        System.out.println("US: "     + us.format(payment));
+        System.out.println("India: "  + india.format(payment));
+        System.out.println("China: "  + china.format(payment));
+        System.out.println("France: " + france.format(payment));
+    }
+}
